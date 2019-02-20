8f8145c9c00c1be128c5488668eafefb1dd03336
Yuzmhar Guillen <yuzmhar.guillen@elemetrica.com>
2019-02-20 00:30:04 -0300

    Pruebas con componentes reutilizables

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        pic={faker.image.avatar()}
-        timeAgo="Today at 6:00PM"
-        comment="Nice blog!"
-      />
-      <CommentDetail
-        author="Alex"
-        pic={faker.image.avatar()}
-        timeAgo="Today at 7:00PM"
-        comment="Nice 2332 blog!"
-      />
-      <CommentDetail
-        author="Jane"
-        pic={faker.image.avatar()}
-        timeAgo="Today at 8:00PM"
-        comment="Nice 2121 blog!"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          pic={faker.image.avatar()}
+          timeAgo="Today at 6:00PM"
+          comment="Nice blog!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          pic={faker.image.avatar()}
+          timeAgo="Today at 7:00PM"
+          comment="Nice 2332 blog!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          pic={faker.image.avatar()}
+          timeAgo="Today at 8:00PM"
+          comment="Nice 2121 blog!"
+        />
+      </ApprovalCard>
     </div>
   );
 };
